3076bda567069d6f777af944e08af13935b942d2
dmitryartemiev <dima77261@gmail.com>
2021-02-05 11:26:25 +0200

    calculator finish

--- js/script.js
+++ js/script.js
@@ -4,24 +4,38 @@ let $resultLane = document.getElementsByClassName("result_lane")[0];
 let $inputLane = document.getElementsByClassName("input_lane")[0];
 
 function insert(num) {
-  $resultLane.value += num;
+    $resultLane = document.getElementsByClassName("result_lane")[0];
+    $resultLane.value += num;
 }
 
 function operation(value) {
-  $inputLane.textContent += $resultLane.value;
-  $inputLane.textContent += value;
-  $resultLane.value = "";
+    $inputLane.textContent += $resultLane.value;
+    $inputLane.textContent += value;
+    $resultLane.value = "";
 }
 
 function clean() {
-  $inputLane.textContent = "";
-  $resultLane.value = "";
+    $inputLane.textContent = "";
+    $resultLane.value = "0";
 }
 
 function equal() {
-  $inputLane.textContent += $resultLane.value;
-  $resultLane.value = eval($inputLane.textContent);
-  $resultLane.value = Number($resultLane.value);
-  console.log(Number.isInteger($resultLane.value));
-  $inputLane.textContent = "";
+    $inputLane.textContent += $resultLane.value;
+    $resultLane.value = eval($inputLane.textContent);
+    $resultLane.value = Number($resultLane.value);
+    console.log(Number.isInteger($resultLane.value));
+
+    if (Number.isInteger($resultLane.value)) {
+        $resultLane.value = Number($resultLane.value).toFixed(2);
+    }
+
+    $inputLane.textContent = "";
+}
+
+function percent() {
+    $resultLane.value = Number($resultLane.value) / 100;
+}
+
+function toggle() {
+    $resultLane.value = Number($resultLane.value) * -1;
 }
